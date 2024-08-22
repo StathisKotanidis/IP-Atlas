ebe620f27e558e1b7f2ecd624132a8ce5da4ea00
StathisKotanidis <stathiskotanidhs@gmail.com>
2024-08-22 20:10:38 +0300

    starting files

---
+++ main.js
@@ -0,0 +1,15 @@
+// fetch(
+//   "https://api.ipgeolocation.io/ipgeo?apiKey=b023e6d115294256a31b469bec5011d3&ip=31.152.204.132"
+// )
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw new Error("Network response was not ok " + response.statusText);
+//     }
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((error) => {
+//     console.error("There has been a problem with your fetch operation:", error);
+//   });
