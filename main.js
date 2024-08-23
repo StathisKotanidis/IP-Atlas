8cabcad7560ac7da8378b075b32f3ba09431a30d
StathisKotanidis <stathiskotanidhs@gmail.com>
2024-08-23 19:17:09 +0300

    first design idea

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
-// fetch(
-//   "https://api.ipgeolocation.io/ipgeo?apiKey=b023e6d115294256a31b469bec5011d3&ip=31.152.204.132"
-// )
-//   .then((response) => {
-//     if (!response.ok) {
-//       throw new Error("Network response was not ok " + response.statusText);
-//     }
-//     return response.json();
-//   })
-//   .then((data) => {
-//     console.log(data);
-//   })
-//   .catch((error) => {
-//     console.error("There has been a problem with your fetch operation:", error);
-//   });
+const searchBtn = document.querySelector("#search-button");
+
+searchBtn.addEventListener("click", getIpData);
+
+function getIpData() {
+  // fetch(
+  //   "https://api.ipgeolocation.io/ipgeo?apiKey=b023e6d115294256a31b469bec5011d3&ip=31.152.204.132"
+  // )
+  //   .then((response) => {
+  //     if (!response.ok) {
+  //       throw new Error("Network response was not ok " + response.statusText);
+  //     }
+  //     return response.json();
+  //   })
+  //   .then((data) => {
+  //     console.log(data);
+  //   })
+  //   .catch((error) => {
+  //     console.error("There has been a problem with your fetch operation:", error);
+  //   });
+}
